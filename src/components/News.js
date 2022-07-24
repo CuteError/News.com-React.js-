6a4a46c675fb9b6a4fe839303181341c360f6c68
Pushpendra Shrivastav <shrivastavsumit15@gmail.com>
2022-07-24 10:25:53 +0530

    News Update

--- src/components/News.js
+++ src/components/News.js
@@ -16,7 +16,7 @@ const News = (props) => {
     }
 
     const updateNews = async () => {
-        props.setProgress(10);
+        // props.setProgress(10);
         const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=${props.apiKey}&page=${page}&pageSize=${props.pageSize}`;
         setLoading(true)
         let data = await fetch(url);
